f30d7917190475f1aaf2ae1f36a206c780263026
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-06-15 09:51:39 -0400

    added firebase for save

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
+import firebase from 'firebase/app';
+import database from 'firebase/database';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBPxZ86IT1COx9jsrZsQ_twY1xP6yOWg-c",
+  authDomain: "rnhprog81102018.firebaseapp.com",
+  databaseURL: "https://rnhprog81102018.firebaseio.com",
+  projectId: "rnhprog81102018",
+  storageBucket: "rnhprog81102018.appspot.com",
+  messagingSenderId: "120108595303"
+};
+firebase.initializeApp(config);
 
 export default class App extends React.Component {
-  aTodos = [];
+  oTodos = {};
   constructor(props) {
     super(props);
-    this.state = {todo: ''};
+    this.state = { todo: '' };
+    fetch("https://rnhprog81102018.firebaseio.com/tasks.json")
+    .then((oData) =>{
+      oData.json().then((data) => this.oTodos = data);
+    }
+
+    )
+    .catch((err) => {
+      console.log(err);
+    }
+
+    );
   }
 
-  updateText(event){
-//    console.log(this.state);
-//    alert("text box: " + this.state.todo);
+  updateText(event) {
+    //    console.log(this.state);
+    //    alert("text box: " + this.state.todo);
     this.aTodos.push(this.state.todo);
-    this.setState({todo: ""});
-    this.refs['todo'].focus();
+    let taskID = Math.floor(Math.random() * 1000);
+    firebase.database().ref('tasks/' + taskID).set({
+      name: this.state.todo
+    });
+    this.setState({ todo: "" });
   }
 
   render() {
@@ -23,19 +50,19 @@ export default class App extends React.Component {
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
         {
-        this.aTodos.map((prop, i)=>{
-          return(<Text key={i}>{prop}</Text>)
-        })
+          Object.keys(this.oTodos).map((key, i) => {
+            return (<Text key={i}>{this.oTodos[key]}</Text>)
+          })
 
-      }
+        }
         <TextInput
-        ref='todo'
-          style={{height: 40}}
+          ref='todo'
+          style={{ height: 40 }}
           placeholder="Type your text here!"
           value={this.state.todo}
-          onChangeText={(text) => this.setState({todo: text})}
+          onChangeText={(text) => this.setState({ todo: text })}
           onSubmitEditing={() => this.updateText()}
-          autoFocus = {true}
+          autoFocus={true}
         />
       </View>
     );
